916b1e4cb71bacd5440790a4d5943de3e376ab07
Eric Tung <libpronet@gmail.com>
2019-02-09 20:33:55 +0800

    Pre-upgrade ver-0.9.0 to ver-0.9.1

--- src/pro/pro_util/pro_version.h
+++ src/pro/pro_util/pro_version.h
@@ -23,11 +23,11 @@
 ////
 
 /*
- * 0.9.0
+ * 0.9.1
  */
 #define PRO_VER_MAJOR 0
 #define PRO_VER_MINOR 9
-#define PRO_VER_PATCH 0
+#define PRO_VER_PATCH 1
 
 /////////////////////////////////////////////////////////////////////////////
 ////
