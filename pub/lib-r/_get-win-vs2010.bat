8f112d1abc01bfdfb3752b04022fbcbd571a9705
Eric Tung <libpronet@gmail.com>
2019-02-17 14:56:07 +0800

    Remove test*.map, test*.pdb

--- pub/lib-r/_get-win-vs2010.bat
+++ pub/lib-r/_get-win-vs2010.bat
@@ -13,4 +13,22 @@ copy /y %THIS_DIR%..\..\build\windows-vs2010\_release64x\*.lib %THIS_DIR%windows
 copy /y %THIS_DIR%..\..\build\windows-vs2010\_release64x\*.map %THIS_DIR%windows-vs2010\x86_64\
 copy /y %THIS_DIR%..\..\build\windows-vs2010\_release64x\*.pdb %THIS_DIR%windows-vs2010\x86_64\
 
+del %THIS_DIR%windows-vs2010\x86\test_msg_client.map
+del %THIS_DIR%windows-vs2010\x86\test_msg_client.pdb
+del %THIS_DIR%windows-vs2010\x86\test_rtp.map
+del %THIS_DIR%windows-vs2010\x86\test_rtp.pdb
+del %THIS_DIR%windows-vs2010\x86\test_tcp_server.map
+del %THIS_DIR%windows-vs2010\x86\test_tcp_server.pdb
+del %THIS_DIR%windows-vs2010\x86\test_tcp_client.map
+del %THIS_DIR%windows-vs2010\x86\test_tcp_client.pdb
+
+del %THIS_DIR%windows-vs2010\x86_64\test_msg_client.map
+del %THIS_DIR%windows-vs2010\x86_64\test_msg_client.pdb
+del %THIS_DIR%windows-vs2010\x86_64\test_rtp.map
+del %THIS_DIR%windows-vs2010\x86_64\test_rtp.pdb
+del %THIS_DIR%windows-vs2010\x86_64\test_tcp_server.map
+del %THIS_DIR%windows-vs2010\x86_64\test_tcp_server.pdb
+del %THIS_DIR%windows-vs2010\x86_64\test_tcp_client.map
+del %THIS_DIR%windows-vs2010\x86_64\test_tcp_client.pdb
+
 pause
