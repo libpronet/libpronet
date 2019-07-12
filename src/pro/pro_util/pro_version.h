0a306dd2d53237cf7e340c01338534d397ebbf32
Eric Tung <libpronet@gmail.com>
2019-07-12 14:41:22 +0800

    ver-1.0.0 to ver-1.0.1

--- src/pro/pro_util/pro_version.h
+++ src/pro/pro_util/pro_version.h
@@ -23,11 +23,11 @@
 ////
 
 /*
- * 1.0.0
+ * 1.0.1
  */
 #define PRO_VER_MAJOR 1
 #define PRO_VER_MINOR 0
-#define PRO_VER_PATCH 0
+#define PRO_VER_PATCH 1
 
 /////////////////////////////////////////////////////////////////////////////
 ////
