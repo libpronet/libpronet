1b4df2c4a2e6603d5f6d4390817700295fbc42b5
Eric Tung <libpronet@gmail.com>
2021-08-22 14:00:38 +0800

    Ver-2.0.1

--- pub/inc/pronet/pro_version.h
+++ pub/inc/pronet/pro_version.h
@@ -23,11 +23,11 @@
 ////
 
 /*
- * 2.0.0
+ * 2.0.1
  */
 #define PRO_VER_MAJOR 2
 #define PRO_VER_MINOR 0
-#define PRO_VER_PATCH 0
+#define PRO_VER_PATCH 1
 
 /////////////////////////////////////////////////////////////////////////////
 ////
