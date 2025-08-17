59cb69d258da8acd1502363d4796dfa185dc2b94
Eric Tung <libpronet@gmail.com>
2025-08-17 15:01:23 +0800

    Ver-2.4.1

--- src/pronet/pro_util/pro_version.h
+++ src/pronet/pro_util/pro_version.h
@@ -23,11 +23,11 @@
 ////
 
 /*
- * 2.4.0
+ * 2.4.1
  */
 #define PRO_VER_MAJOR 2
 #define PRO_VER_MINOR 4
-#define PRO_VER_PATCH 0
+#define PRO_VER_PATCH 1
 
 /////////////////////////////////////////////////////////////////////////////
 ////
